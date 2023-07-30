2b593b10e0f82073fc1d72a0d0196f9e53da5a59
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-07-30 12:03:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ def data_generator(dataset, batch_size, time_slice, datapath, shuffle=True):
         shd_train = data_mod(shd_train['spikes'], shd_train['labels'], batch_size = batch_size, step_size = time_slice, input_size = 700, max_time = 1.37)
         shd_test = data_mod(shd_test['spikes'], shd_test['labels'], batch_size = batch_size, step_size = time_slice, input_size = 700, max_time = 1.37)
         
-        train_loader = shd_train[:int(0.9 * len(shd_train))]
-        val_loader = shd_train[int(0.9 * len(shd_train)):]
+        train_loader = shd_train[:int(0.95 * len(shd_train))]
+        val_loader = shd_train[int(0.95 * len(shd_train)):]
         test_loader = shd_test
         n_classes = 20
-        seq_length = 100
+        seq_length = time_slice
         input_channels = 700
 
     else:
