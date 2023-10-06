1289a7f09ae07cddbaf58e29d7edf50f38b8a7b3
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-10-06 15:42:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -260,7 +260,7 @@ epochs = args.epochs
 prun_rate2, prun_rate3 = args.prun_rate[0], args.prun_rate[1]
 reg_rate2, reg_rate3 = args.reg_rate[0], args.reg_rate[1]
 T = args.t_num
-START = 20                                       # Pruning starts at this epoch
+START = 5                                       # Pruning starts at this epoch
 first_update = False
 named_params = get_stats_named_params(model)
 
@@ -331,9 +331,9 @@ for epoch in range(1, epochs + 1):
             # print('Test Loss:', test_loss, end = '\t')
             # print('Test Accuracy:', test_acc.item())
 
-        temp = curr_w2
+        print(curr_w2[:6])
         curr_w2, R_pos_2, R_neg_2, C_pos_2, C_neg_2, N_pos_2, N_neg_2, N2 = synaptic_constraint(curr_w2, prev_w2, R_pos_2, R_neg_2, C_pos_2, C_neg_2, N_pos_2, N_neg_2, N2, T)
-        if torch.equal(temp, curr_w2): print('skibidi')
+        print(curr_w2[:6])
         curr_w3, R_pos_3, R_neg_3, C_pos_3, C_neg_3, N_pos_3, N_neg_3, N3 = synaptic_constraint(curr_w3, prev_w3, R_pos_3, R_neg_3, C_pos_3, C_neg_3, N_pos_3, N_neg_3, N3, T)
 
         if epoch > START:
