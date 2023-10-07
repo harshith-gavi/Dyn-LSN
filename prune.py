1e22b28b019e61a6a6670808ff58d4848b646347
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-10-07 20:23:38 +0200

    Update prune.py

--- prune.py
+++ prune.py
@@ -71,10 +71,10 @@ def plasticity(clw, nlw, R_pos, R_neg, prun_rate, reg_rate, T, model, layer, epo
     N_cl = no_neu.sum().item()
     print('Number of Neurons in {layer} Layer: ', N_cl)
     if layer == 'h1':
+         print(nlw)
          no_neu = torch.all(nlw != 0, dim=0)
          N_nl = no_neu.sum().item()
     elif layer == 'h2':
-         print(clw)
          N_nl = 20
     print('Number of Neurons in {layer} Layer: ', N_nl)
 
