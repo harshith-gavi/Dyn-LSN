4f56d356a16b6bf41b8a21438a1f404b5db3bcef
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-11-05 17:36:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -285,7 +285,6 @@ for epoch in range(1, epochs + 1):
         # Previous Epoch Weights
         prev_w2 = model.network.layer1_x.weight.data.T
         prev_w3 = model.network.layer2_x.weight.data.T
-        print('if1', torch.count_nonzero(prev_w2).item())
 
         # Training
         train(epoch, args, train_loader, n_classes, model, named_params, 1, progress_bar)  
@@ -325,7 +324,6 @@ for epoch in range(1, epochs + 1):
             curr_w2, prun_rate2, reg_rate2, T_g2, N_n, syns = plasticity(curr_w2, curr_w3, R_pos_2, R_neg_2, prun_rate2, reg_rate2, T, Tg2, model, 'h1', N_n, lr, epoch)
             syns_h1.append(syns)
             model.network.layer1_x.weight.data = curr_w2.T
-            print('if2', torch.count_nonzero(model.network.layer1_x.weight.data).item())
             curr_w3, prun_rate3, reg_rate3, T_g3, N_n, syns = plasticity(curr_w3, curr_w4, R_pos_3, R_neg_3, prun_rate3, reg_rate3, T, Tg3, model, 'h2', N_n, lr, epoch)
             syns_h2.append(syns)
             model.network.layer2_x.weight.data = curr_w3.T
