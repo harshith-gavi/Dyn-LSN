69fed71546c09944cf37932ba908285c1f176751
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-11-07 15:54:38 +0100

    Update neuroplasticity.py

--- neuroplasticity.py
+++ neuroplasticity.py
@@ -88,6 +88,7 @@ def plasticity(clw, nlw, R_pos, R_neg, prun_rate, reg_rate, T, T_g, model, layer
     ncl = torch.count_nonzero(clw).item() / factor_
     nnl =  (torch.count_nonzero(nlw).item() / 256) if layer == 'h1' else 20 if layer == 'h2' else None
     prun_rate += (d * ncl / nnl)
+    if torch.is_tensor(prun_rate): prun_rate = prun_rate.item()
     if prun_rate > 0.99: prun_rate = 0.99
 
     #---------------------------------- Regeneration ------------------------------------#
