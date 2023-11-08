66d5fc5cae358ef5bc91d16cc0c6d9901b8f2ed9
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-11-08 20:07:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ parser.add_argument('--rho', type=float, default=0.0, help='Weight update parame
 parser.add_argument('--lmda', type=float, default=1.0, help='Regularisation strength (Lambda)')
 
 parser.add_argument('--prun_rate', nargs='+', type=float, default=[0.05, 0.05], help = 'Pruning rate per layer')
-parser.add_argument('--reg_rate', nargs='+', type=float, default=[0.01, 0.01], help = 'Regeneration rate per layer')
+parser.add_argument('--reg_rate', nargs='+', type=float, default=[0.05, 0.05], help = 'Regeneration rate per layer')
 parser.add_argument('--t_num', type=int, default=5, help = 'Plasticity Threshold')
 parser.add_argument('--seed', type=int, default=1111, help='Random seed')
 
